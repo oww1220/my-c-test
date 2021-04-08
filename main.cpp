083326bbcb75dc013fcf8a20fc5496f915d279f3
이동걸 <oww@mz.co.kr>
2021-04-08 18:14:58 +0900

    백준 !

--- main.cpp
+++ main.cpp
@@ -4,24 +4,36 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <cstring>
 
 int main()
 {
-    int t;
+    int t, calculate=0, total=0;
     scanf("%d", &t);
 
-    char test[t][81];
+    char test[t][81]={0};
 
     for(int i=0; i < t; i++) {
         scanf("%s", test[i]);
-
-        printf("%s", test[i]);
     }
 
     for(int i=0; i < t; i++) {
+        printf("%s\n", test[i]);
+        for(int j=0; j < strlen(test[i]); j++) {
+            //printf("%c\n", test[i][j]);
+            
+            if(test[i][j] == 'o') {
+                calculate++;
+                total= total + calculate;
+            }
+            else {
+                calculate = 0;
+            }
 
-
-        printf("%d", strlen(test[i]));
+            //ptr++;
+            
+        }
+        printf("%d\n", total);
     }
     
 
