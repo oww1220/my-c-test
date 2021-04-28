3d72578dd3de9266c86f3dcf9941b1492137931d
lee donggirl <oww@mz.co.kr>
2021-04-29 00:32:38 +0900

    알고리즘 update!

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,28 @@
-overlap
+
+#include <stdio.h>
+#include <string.h>
+#include <cstring>
+#include <math.h>
+#include <stdlib.h>    // malloc, free 함수가 선언된 헤더 파일
+#include <ctype.h>
+
+int main() {
+
+    int flag=0, result=1;
+    char inputString[1000001]={0};
+
+    scanf("%[^\n]s", inputString);
+
+    for(int i=0;i<strlen(inputString);i++) {
+
+        if(inputString[i] == ' '){
+            flag=1;
+        }
+        else{
+            if(flag==1) result++;
+            flag=0;
+        }
+    }
+    printf("%d", result);
+    return 0;
+}
